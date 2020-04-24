0acbf1b343407ea885c5c1b855b43f996e0d9766
barun <barun@DESKTOP-S5KMPK9>
2020-04-24 17:59:07 +0900

    Created angular project and completed the feature 1.

---
+++ angular-bookstore/src/app/common/book.ts
@@ -0,0 +1,12 @@
+export class Book {
+
+  sku: string;
+  name: string;
+  description: string;
+  unitPrice: number;
+  imageUrl: String;
+  active: boolean;
+  unitsInStock: number;
+  createdOn: Date;
+  updatedOn: Date;
+}
--- onlinebookstore/src/main/java/com/projects/crud/repository/BookRepository.java
+++ onlinebookstore/src/main/java/com/projects/crud/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package com.projects.crud.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.projects.crud.entity.Book;
 
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
