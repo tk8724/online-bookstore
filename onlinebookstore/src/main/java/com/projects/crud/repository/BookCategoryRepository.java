709bb238a71a115f882c8da453d57ec1192aff24
barun <barun@DESKTOP-S5KMPK9>
2020-04-24 13:19:31 +0900

    Completed the backend for feature one.

---
+++ onlinebookstore/src/main/java/com/projects/crud/repository/BookCategoryRepository.java
@@ -0,0 +1,11 @@
+package com.projects.crud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.projects.crud.entity.BookCategory;
+
+@RepositoryRestResource(collectionResourceRel="bookCategory" , path="book-category")
+public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
+
+}
