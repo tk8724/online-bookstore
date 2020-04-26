abd11ba6d91a42c4515f9647f31d63c7fce0418c
barun <barun@DESKTOP-S5KMPK9>
2020-04-26 09:20:29 +0900

    Completed feature 2

--- angular-bookstore/src/app/components/book-list/book-list.component.ts
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -1,40 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../../common/book';
 import { BookService } from '../../services/book.service';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
-  //templateUrl: './book-list.component.html',
-  templateUrl: './book-grid.component.html',
+  templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
 
   books: Book[];
-  currentCategoryId: number;
 
-  constructor(private _bookService: BookService,
-              private _activatedRoute: ActivatedRoute) { }
+  constructor(private _bookService: BookService) { }
 
   ngOnInit() {
-    this._activatedRoute.paramMap.subscribe(()=>{
-        this.listBooks();
-    })
+    this.listBooks()
   }
-
   listBooks(){
-
-    const hasCategoryId: boolean = this._activatedRoute.snapshot.paramMap.has('id');
-
-    if(hasCategoryId){
-      this.currentCategoryId = +this._activatedRoute.snapshot.paramMap.get('id');
-    }else {
-      this.currentCategoryId = 1;
-    }
-
-    this._bookService.getBooks(this.currentCategoryId).subscribe(
+    this._bookService.getBooks().subscribe(
       data => this.books = data
+      
     )
   }
 }
