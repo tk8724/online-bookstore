bfaf25b58b3b62eb29bd36262469d357736e9c19
tk8724 <tonykim8724@naver.com>
2020-04-27 10:45:30 +0900

    Completed feature 4

--- onlinebookstore/src/main/java/com/projects/crud/repository/BookCategoryRepository.java
+++ onlinebookstore/src/main/java/com/projects/crud/repository/BookCategoryRepository.java
@@ -2,8 +2,10 @@ package com.projects.crud.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.projects.crud.entity.BookCategory;
+
 
 @RepositoryRestResource(collectionResourceRel="bookCategory" , path="book-category")
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
--- onlinebookstore/src/main/java/com/projects/crud/repository/BookRepository.java
+++ onlinebookstore/src/main/java/com/projects/crud/repository/BookRepository.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.projects.crud.entity.Book;
 
-@CrossOrigin("http://localhost:4200")
+
 public interface BookRepository extends JpaRepository<Book, Long> {
 	
 	@RestResource(path ="categoryid")
